c97647073284b5d5973f44a8e5a195f6b549af33
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-02-07 17:35:18 +0100

    feat: final touch

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,26 @@ import Code from "@/components/Code";
 export default function Home() {
   return (
     <>
-      <Code code="let a = 1 + 4 // [!code --]" filename="index.js" />
       <Code
-        code="console.log('Hello, world!') // [!code ++]"
-        lang="typescript"
-        filename="index.ts"
-      />
-      <Code
-        code={`fn main() { println!(\"Hello, world!\"); } // [!code highlight]`}
-        lang="rust"
-        theme="github-dark"
-        filename="main.rs"
+        code={`return (// [!code ++]
+  <div className="rounded-lg bg-gradient-to-r from-sky-300 to-sky-500 p-4 !pr-0 md:p-8 lg:p-12 [&>pre]:rounded-none max-w-xl">// [!code --]
+    <div className="overflow-hidden rounded-s-lg">// [!code highlight]
+      <div className="flex items-center justify-between bg-gradient-to-r from-neutral-900 to-neutral-800 py-2 pl-2 pr-4 text-sm">
+        <span className="-mb-[calc(0.5rem+2px)] rounded-t-lg border-2 border-white/5 border-b-neutral-700 bg-neutral-800 px-4 py-2 ">
+          {filename}
+        </span>
+      </div>
+      <div
+        className="border-t-2 border-neutral-700 text-sm [&>pre]:overflow-x-auto [&>pre]:!bg-neutral-900 [&>pre]:py-3 [&>pre]:pl-4 [&>pre]:pr-5 [&>pre]:leading-snug [&_code]:block [&_code]:w-fit [&_code]:min-w-full"
+        dangerouslySetInnerHTML={{ __html: html }}
+      ></div>
+    </div>
+  </div>
+);
+`}
+        lang="tsx"
+        theme="ayu-dark"
+        filename="app/page.tsx"
       />
     </>
   );
--- components/Code.tsx
+++ components/Code.tsx
@@ -24,18 +24,18 @@ export default async function Code({
   });
 
   return (
-    <div>
-      <div className="bg-neutral-800">
-        {filename && (
-          <div className="bg-neutral-900 text-sm inline-flex py-2 px-4">
+    <div className="rounded-lg bg-gradient-to-r from-sky-200 to-sky-400 p-4 !pr-0 md:p-8 lg:p-12 [&>pre]:rounded-none max-w-xl">
+      <div className="overflow-hidden rounded-s-lg">
+        <div className="flex items-center justify-between bg-gradient-to-r from-neutral-900 to-neutral-800 py-2 pl-2 pr-4 text-sm">
+          <span className="-mb-[calc(0.5rem+2px)] rounded-t-lg border-2 border-white/5 border-b-neutral-700 bg-neutral-800 px-4 py-2 ">
             {filename}
-          </div>
-        )}
+          </span>
+        </div>
+        <div
+          className="border-t-2 border-neutral-700 text-sm [&>pre]:overflow-x-auto [&>pre]:!bg-neutral-900 [&>pre]:py-3 [&>pre]:pl-4 [&>pre]:pr-5 [&>pre]:leading-snug [&_code]:block [&_code]:w-fit [&_code]:min-w-full"
+          dangerouslySetInnerHTML={{ __html: html }}
+        ></div>
       </div>
-      <div
-        className="[&_.highlighted]:bg-blue-700"
-        dangerouslySetInnerHTML={{ __html: html }}
-      ></div>
     </div>
   );
 }
