084fdb589d0ff00dd255ca7055f2009482308c1e
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-02-07 17:22:57 +0100

    feat: integrate shiki

--- components/Code.tsx
+++ components/Code.tsx
@@ -1,3 +1,10 @@
-export default function Code() {
-  return <div>Code</div>;
+import { codeToHtml } from "shiki";
+
+export default async function Code() {
+  const html = await codeToHtml("const a = 1 + 3", {
+    lang: "javascript",
+    theme: "nord",
+  });
+
+  return <div dangerouslySetInnerHTML={{ __html: html }}></div>;
 }
